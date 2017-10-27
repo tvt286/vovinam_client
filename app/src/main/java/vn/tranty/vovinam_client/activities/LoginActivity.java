b008f898a5e66856f1f9a120861ed282a71603f3
Tran Ty <tranty.it286@gmail.com>
2017-10-27 16:49:26 +0700

    toast

--- app/src/main/java/vn/tranty/vovinam_client/activities/LoginActivity.java
+++ app/src/main/java/vn/tranty/vovinam_client/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package vn.tranty.vovinam_client.activities;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import vn.tranty.vovinam_client.R;
 import vn.tranty.vovinam_client.interfaces.Response;
+import vn.tranty.vovinam_client.mics.Utils;
 import vn.tranty.vovinam_client.requests.UserRequest;
 
 public class LoginActivity extends AppCompatActivity {
@@ -29,42 +31,50 @@ public class LoginActivity extends AppCompatActivity {
     TextView tvUserName;
     @Bind(R.id.tv_password)
     TextView tvPassword;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
     }
 
     @OnClick(R.id.btn_login)
     void onClickLogin()
     {
-        if (tvPassword.getText().equals("") || tvUserName.getText().equals(""))
+        if (tvPassword.getText().toString().matches("") || tvUserName.getText().toString().matches(""))
         {
-            Toast.makeText(this, "Vui lòng nhập!", Toast.LENGTH_SHORT).show();
-            return;
+            Utils.showToast("Vui lòng nhập tên đăng nhập hoặc mật khẩu!",this);
         }
+        else
+            {
+                progress = new ProgressDialog(getBaseContext());
+                progress = ProgressDialog.show(getBaseContext(), null, "Đang đăng nhập...", true);
 
-        UserRequest.login(tvUserName.getText().toString(), tvPassword.getText().toString(), new Response() {
-            @Override
-            public void onStart() {
+                UserRequest.login(tvUserName.getText().toString(), tvPassword.getText().toString(), new Response() {
+                @Override
+                public void onStart() {
 
-            }
+                }
 
-            @Override
-            public void onSuccess(int error_code, String message, Object obj) {
-                Intent i = new Intent(getBaseContext(), MainActivity.class);
-                startActivity(i);
-                overridePendingTransition(0,0);
-                finish();
-            }
+                @Override
+                public void onSuccess(int error_code, String message, Object obj) {
+                    progress.dismiss();
+                    Intent i = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(0, 0);
+                    finish();
+                }
 
-            @Override
-            public void onFailure() {
-
-            }
-        });
+                @Override
+                public void onFailure() {
+                    progress.dismiss();
+                    Utils.showToast("Vui lòng kiểm tra lại internet!",LoginActivity.this);
+                }
+            });
+        }
 
     }
 
--- app/src/main/java/vn/tranty/vovinam_client/mics/Utils.java
+++ app/src/main/java/vn/tranty/vovinam_client/mics/Utils.java
@@ -1,10 +1,17 @@
 package vn.tranty.vovinam_client.mics;
 
 import android.app.Activity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import vn.tranty.vovinam_client.R;
 
 /**
  * Created by TRUC-SIDA on 10/19/2017.
@@ -18,5 +25,17 @@ public class Utils {
                         Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(
                 activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public static void showToast(String message, Activity context) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View layout = inflater.inflate(R.layout.my_toast, (ViewGroup) context.findViewById(R.id.toast_layout_root));
+        TextView text = (TextView) layout.findViewById(R.id.tv_toast);
+        text.setText(message);
+        Toast toast = new Toast(context.getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM,0,80);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 }
